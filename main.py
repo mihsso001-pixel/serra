52eb9e08c534bcdd4d001916385836134c373b44
mihsso001-pixel <mihsso001@gmail.com>
2026-02-04 05:06:50 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Huu ni mwanzo wa Serra JY
+def serra_salamu():
+    jina = "Seirra"
+    print(f"Habari! Mimi naitwa {jina}, msaidizi wako wa AI.")
+    print("Niko tayari kuanza kazi kwenye kompyuta yako.")
+
+if __name__ == "__main__":
+    serra_salamu()
